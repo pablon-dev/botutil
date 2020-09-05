50117acf3211ad8b7cf019a6dd3b0e1d6783c8c7
pablon <pablon.contact@gmail.com>
2020-09-05 01:07:46 -0400

    New functions included

--- botutil.go
+++ botutil.go
@@ -2,17 +2,55 @@ package botutil
 
 import (
 	"errors"
+	"fmt"
 	"strings"
 )
 
-const slash int = 47
+const slash byte = 47
 
 //Getcommand Recieve command and return parameters
-func Getcommand(text string) (command []string, err error) {
+func Getcommand(text string, separ byte) (command []string, err error) {
 	text = strings.Trim(text, " ")
-	if text[0] != slash {
-		return nil, errors.New("Commands must start with: '/' ")
+	if text[0] != separ {
+		return nil, fmt.Errorf("Commands must start with: '%c' ", separ)
 	}
 	s := strings.Split(text, " ")
 	return s, nil
+}
+
+//GetcommandB Recieve command and arguments and return parameters
+func GetcommandB(comm string, args string) (command []string, err error) {
+	if comm == "" {
+		return nil, errors.New("Not valid Command ")
+	}
+	command = append(command, "/"+comm)
+	if args != "" {
+		s := strings.Split(args, " ")
+		command = append(command, s...)
+	}
+	return command, nil
+}
+
+//SplitIgnore Recieve a string and return an array of substrings without empty space
+func SplitIgnore(text string) (parts []string) {
+	splitF := func(c rune) bool {
+		return c == ' '
+	}
+	parts = strings.FieldsFunc(text, splitF)
+	return
+}
+
+//URLform give the html/markdown form of a given url and body
+func URLform(url, body string) (result string) {
+	result = fmt.Sprintf(`[%s](%s)`, body, url)
+	return
+}
+
+//NewLine append y to x in new Line
+func NewLine(x, y string) string {
+	if x == "" {
+		return y
+	}
+	return x + "\n" + y
+
 }
